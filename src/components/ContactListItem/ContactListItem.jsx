ac6bb0743a9b4fb41fd2df4f8dd7f921da446345
Yurii-Yevsiukov <151449297+Yurii-Yevsiukov@users.noreply.github.com>
2024-07-24 21:51:07 +0300

    Initial commit

---
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import css from './ContactListItem.module.css';
+
+export const ContactListItem = ({ filteredContact, deleteContact }) => {
+  const handleDelete = () => {
+    deleteContact(filteredContact.id);
+  };
+
+  return (
+    <li className={css.contactListItem}>
+      <p>{filteredContact.name}:</p>
+      <p>{filteredContact.number}</p>
+      <button className={css.delBtn} onClick={handleDelete}>
+        Delete
+      </button>
+    </li>
+  );
+};
+
+ContactListItem.propTypes = {
+  filteredContact: PropTypes.object.isRequired,
+  deleteContact: PropTypes.func.isRequired,
+};
